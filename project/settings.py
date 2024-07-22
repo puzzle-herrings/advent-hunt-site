6064414c7ec258bca7f0f201768260a50615a6e5
Jay Qi <jayqi@users.noreply.github.com>
2024-07-22 17:20:11 -0400

    Rearrange some stuff

--- project/settings.py
+++ project/settings.py
@@ -24,6 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
+## Security
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
 
@@ -41,7 +43,7 @@ if RENDER_EXTERNAL_HOSTNAME:
 
 INTERNAL_IPS = env.list("INTERNAL_IPS", [])
 
-# Application definition
+## Applications
 
 INSTALLED_APPS = [
     "django.contrib.admin",
