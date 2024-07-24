6756002542a64390e7a5c7ef72d61ef70fe4105c
Jay Qi <jayqi@users.noreply.github.com>
2024-07-24 02:12:14 -0400

    Reorganize tests

--- huntsite/puzzles/tests/test_models.py
+++ huntsite/puzzles/tests/test_models.py
@@ -1,7 +1,6 @@
 from django.utils import timezone
 import pytest
 
-from huntsite.puzzles.admin import PuzzleAdminForm
 from huntsite.puzzles.factories import PuzzleFactory
 from huntsite.puzzles.models import AdventCalendarEntry, Puzzle
 from huntsite.puzzles.services import guess_submit
@@ -10,17 +9,7 @@ from huntsite.teams.factories import UserFactory
 pytestmark = pytest.mark.django_db
 
 
-@pytest.fixture
-def puzzle():
-    return PuzzleFactory()
-
-
-@pytest.fixture()
-def user():
-    return UserFactory()
-
-
-def test_puzzle_create(puzzle):
+def test_puzzle_create():
     """Creating a puzzle automatically create a advent calendar entry"""
     puzzle = Puzzle.objects.create(
         name="A New Puzzle",
@@ -30,21 +19,24 @@ def test_puzzle_create(puzzle):
     assert AdventCalendarEntry.objects.get(puzzle=puzzle)
 
 
-def test_puzzle_manager_with_calendar_entry(puzzle):
+def test_puzzle_manager_with_calendar_entry():
     """Custom model manager method with_calendar_entry selects related calendar_entry."""
 
-    calendar_entry_field = Puzzle._meta.get_field("calendar_entry")
+    PuzzleFactory()
 
     result_without = Puzzle.objects.first()
+    calendar_entry_field = Puzzle._meta.get_field("calendar_entry")
     assert not calendar_entry_field.is_cached(result_without)
 
     result_with = Puzzle.objects.with_calendar_entry().first()
     assert calendar_entry_field.is_cached(result_with)
 
 
-def test_puzzle_manager_with_solves_by_user(puzzle, user):
+def test_puzzle_manager_with_solves_by_user():
     """Custom model manager method with_solves_by_user annotates returned queryset with
     is_solved."""
+    PuzzleFactory()
+    user = UserFactory()
 
     result_without = Puzzle.objects.first()
     assert not hasattr(result_without, "is_solved")
@@ -66,43 +58,3 @@ def test_available_puzzle_manager():
 
     assert set(Puzzle.objects.all()) == {puzzle_avail, puzzle_not_avail}
     assert set(Puzzle.available.all()) == {puzzle_avail}
-
-
-def test_puzzle_admin_form():
-    """PuzzleAdminForm should handle keep_going_answers_ field correctly."""
-
-    # keep_going_answers_ should be split and saved as keep_going_answers
-    form = PuzzleAdminForm(
-        data={
-            "name": "A New Puzzle",
-            "slug": "a-new-puzzle",
-            "answer": "A NEW ANSWER",
-            "pdf_url": "https://example.com/example.pdf",
-            "available_at": timezone.now(),
-            "keep_going_answers_": "A NEW ANSWER\nANOTHER ANSWER",
-        }
-    )
-    assert form.is_valid()
-    assert form.cleaned_data["keep_going_answers"] == ["A NEW ANSWER", "ANOTHER ANSWER"]
-    puzzle = form.save()
-    assert puzzle.keep_going_answers == ["A NEW ANSWER", "ANOTHER ANSWER"]
-
-    # Initial value populates from existing value of keep_going_answers
-    form2 = PuzzleAdminForm(instance=puzzle)
-    assert form2["keep_going_answers_"].initial == "A NEW ANSWER\nANOTHER ANSWER"
-
-    # Setting keep_going_answers_ to empty string should clear keep_going_answers
-    form3 = PuzzleAdminForm({}, instance=puzzle)
-    form3.data = (
-        form3.data
-        | {field.name: form3[field.name].initial for field in form}
-        | {"keep_going_answers_": ""}
-    )
-    assert form3.is_valid(), form3.errors
-    assert form3.cleaned_data["keep_going_answers"] == []
-    puzzle = form3.save()
-    assert puzzle.keep_going_answers == []
-
-
-def test_puzzle_detail_view(puzzle, client):
-    pass
