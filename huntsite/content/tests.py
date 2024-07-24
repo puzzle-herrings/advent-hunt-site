4f248b3d630f819158a51a10703171ab58f0561f
Jay Qi <jayqi@users.noreply.github.com>
2024-07-23 23:12:51 -0400

    Use factory

--- huntsite/content/tests.py
+++ huntsite/content/tests.py
@@ -1,16 +1,16 @@
 import pytest
 
-from huntsite.content.models import AboutEntry
+from huntsite.content.factories import AboutEntryFactory
 
 pytestmark = pytest.mark.django_db
 
 
 def test_about_page(client):
-    AboutEntry.objects.create(
+    AboutEntryFactory(
         title="But do you recall the most famous reindeer of all?",
         content="Rudolph the red-nosed reindeer had a very shiny nose.",
     )
-    AboutEntry.objects.create(
+    AboutEntryFactory(
         title="Won't you guide my sleigh tonight?",
         content="Rudolph the Red-Nosed Reindeer, you'll go down in history.",
     )
