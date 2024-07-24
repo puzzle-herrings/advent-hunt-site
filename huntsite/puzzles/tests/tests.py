4b87b3aeb5e34d5da479e423e320f14d0f5ee496
Jay Qi <jayqi@users.noreply.github.com>
2024-07-24 01:31:42 -0400

    Test for puzzle admin form

--- huntsite/puzzles/tests/tests.py
+++ huntsite/puzzles/tests/tests.py
@@ -1,6 +1,7 @@
 from django.utils import timezone
 import pytest
 
+from huntsite.puzzles.admin import PuzzleAdminForm
 from huntsite.puzzles.factories import PuzzleFactory
 from huntsite.puzzles.models import AdventCalendarEntry, Puzzle
 from huntsite.puzzles.services import guess_submit
@@ -65,6 +66,42 @@ def test_available_puzzle_manager():
 
     assert set(Puzzle.objects.all()) == {puzzle_avail, puzzle_not_avail}
     assert set(Puzzle.available.all()) == {puzzle_avail}
+
+
+def test_puzzle_admin_form():
+    """PuzzleAdminForm should handle keep_going_answers_ field correctly."""
+
+    # keep_going_answers_ should be split and saved as keep_going_answers
+    form = PuzzleAdminForm(
+        data={
+            "name": "A New Puzzle",
+            "slug": "a-new-puzzle",
+            "answer": "A NEW ANSWER",
+            "pdf_url": "https://example.com/example.pdf",
+            "available_at": timezone.now(),
+            "keep_going_answers_": "A NEW ANSWER\nANOTHER ANSWER",
+        }
+    )
+    assert form.is_valid()
+    assert form.cleaned_data["keep_going_answers"] == ["A NEW ANSWER", "ANOTHER ANSWER"]
+    puzzle = form.save()
+    assert puzzle.keep_going_answers == ["A NEW ANSWER", "ANOTHER ANSWER"]
+
+    # Initial value populates from existing value of keep_going_answers
+    form2 = PuzzleAdminForm(instance=puzzle)
+    assert form2["keep_going_answers_"].initial == "A NEW ANSWER\nANOTHER ANSWER"
+
+    # Setting keep_going_answers_ to empty string should clear keep_going_answers
+    form3 = PuzzleAdminForm({}, instance=puzzle)
+    form3.data = (
+        form3.data
+        | {field.name: form3[field.name].initial for field in form}
+        | {"keep_going_answers_": ""}
+    )
+    assert form3.is_valid(), form3.errors
+    assert form3.cleaned_data["keep_going_answers"] == []
+    puzzle = form3.save()
+    assert puzzle.keep_going_answers == []
 
 
 def test_puzzle_detail_view(puzzle, client):
