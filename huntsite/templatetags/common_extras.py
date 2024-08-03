219de94d8dfabe70f22cabd938fc3b17b196df0d
Jay Qi <jayqi@users.noreply.github.com>
2024-08-03 01:49:06 -0400

    Remove extra filter

--- huntsite/templatetags/common_extras.py
+++ huntsite/templatetags/common_extras.py
@@ -13,9 +13,3 @@ def dictget(dict, key):
 @register.filter()
 def split(value):
     return value.split()
-
-
-@register.filter()
-def mydate(value):
-    assert isinstance(value, datetime.datetime), type(value)
-    return "foo"
