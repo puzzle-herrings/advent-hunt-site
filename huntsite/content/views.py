a78d2c44ffb4949f24d1e52a937078fafc2dceb4
Jay Qi <jayqi@users.noreply.github.com>
2024-08-18 18:16:55 -0400

    Lint

--- huntsite/content/views.py
+++ huntsite/content/views.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.decorators import login_required
 from django.http import Http404
 from django.template.response import TemplateResponse
 from django.views.decorators.http import require_safe
