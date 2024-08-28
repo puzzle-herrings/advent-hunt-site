2d93b4edf006608d270d36f8ab3a76feb2da3bb2
Jay Qi <jayqi@users.noreply.github.com>
2024-08-27 23:33:38 -0400

    Filter out nonactive users

--- huntsite/teams/tests/test_views.py
+++ huntsite/teams/tests/test_views.py
@@ -9,12 +9,18 @@ def test_team_list_view(client):
     UserFactory(team_name="Team 1")
     UserFactory(team_name="Team 2")
     UserFactory(team_name="Team 3")
+    UserFactory(team_name="Team Test", is_tester=True)
+    UserFactory(team_name="Team Admin", is_staff=True)
+    UserFactory(team_name="Team Deactivated", is_active=False)
 
     response = client.get("/teams/")
     assert response.status_code == 200
     assert "Team 1" in response.content.decode()
     assert "Team 2" in response.content.decode()
     assert "Team 3" in response.content.decode()
+    assert "Team Test" not in response.content.decode()
+    assert "Team Admin" not in response.content.decode()
+    assert "Team Deactivated" not in response.content.decode()
 
 
 def test_team_detail_view(client):
