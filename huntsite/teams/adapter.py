f2fffc72bc5edfe96d928448761507fbf2d70f3c
Jay Qi <2721979+jayqi@users.noreply.github.com>
2024-09-03 02:17:17 -0400

    Fix errors with team name length (#52)
    Co-authored-by: Jay Qi <jayqi@users.noreply.github.com>

--- huntsite/teams/adapter.py
+++ huntsite/teams/adapter.py
@@ -7,3 +7,10 @@ class AccountAdapter(DefaultAccountAdapter):
         if getattr(settings, "ACCOUNT_DISABLE_REGISTRATION", False):
             return False
         return super().is_open_for_signup(request)
+
+    def save_user(self, request, user, form, commit=True):
+        user = super().save_user(request, user, form, commit=False)
+        user.team_name = form.cleaned_data.get("team_name", "").strip()
+        user.full_clean()
+        if commit:
+            user.save()
