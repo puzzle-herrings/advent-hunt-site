1fee4660f6a0741138d1166ac5dad8b734679069
Jay Qi <2721979+jayqi@users.noreply.github.com>
2024-10-19 13:31:33 -0400

    Add attributions page (#56)
    Co-authored-by: Jay Qi <jayqi@users.noreply.github.com>

--- huntsite/content/urls.py
+++ huntsite/content/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("about/", views.about_page, name="about"),
     path("story/", views.story_page, name="story"),
     path("story/victory/", views.victory_page, name="victory"),
+    path("attributions/", views.attributions_page, name="attributions"),
 ]
