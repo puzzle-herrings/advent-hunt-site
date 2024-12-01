6afcf126279138918d3061d3bcf4220fe9779f26
Jay Qi <2721979+jayqi@users.noreply.github.com>
2024-12-01 02:11:05 -0500

    Batch bulk emails (#65)
    * Email in batch; select only active user emails
    
    * Don't exclude admin and testers
    
    ---------
    
    Co-authored-by: Jay Qi <jayqi@users.noreply.github.com>

--- huntsite/teams/tests/test_services.py
+++ huntsite/teams/tests/test_services.py
@@ -1,7 +1,16 @@
+from textwrap import dedent
+
+from django.conf import settings
+from django.core import mail
 import pytest
 
-from huntsite.teams.factories import NO_EMAIL_ADDRESSES, UserFactory
-from huntsite.teams.services import user_deactivate
+from huntsite.teams.factories import NO_EMAIL_ADDRESSES, EmailAddressFactory, UserFactory
+from huntsite.teams.services import (
+    _unmark,
+    email_address_select_all_active,
+    send_email,
+    user_deactivate,
+)
 
 pytestmark = pytest.mark.django_db
 
@@ -35,3 +44,63 @@ def test_user_deactivate(client):
     assert user3.is_active
     user_deactivate(user3)
     assert not user3.is_active
+
+
+def test_active_email_select():
+    user1_email = EmailAddressFactory(user=UserFactory())
+    user2 = UserFactory()
+    user2_email1 = EmailAddressFactory(user=user2)
+    user2_email2 = EmailAddressFactory(user=user2)
+    admin_email = EmailAddressFactory(user=UserFactory(is_staff=True))
+    deactivated_email = EmailAddressFactory(user=UserFactory(is_active=False))
+    tester_email = EmailAddressFactory(user=UserFactory(is_tester=True))
+
+    emails = email_address_select_all_active()
+    assert user1_email in emails
+    assert user2_email1 in emails
+    assert user2_email2 in emails
+    assert admin_email in emails
+    assert tester_email in emails
+    assert deactivated_email not in emails
+
+
+def test_markdown_unmark():
+    message = "foo [bar](https://www.adventhunt.com) baz"
+    expected = "foo bar (https://www.adventhunt.com) baz"
+    assert _unmark(message) == expected
+
+    message = dedent("""\
+    Here's a first **paragraph**. There's a [link](https://www.adventhunt.com) in it.
+
+    And here's a _second_ paragraph.
+    """)
+    expected = dedent("""\
+    Here's a first paragraph. There's a link (https://www.adventhunt.com) in it.
+
+    And here's a second paragraph.
+    """).strip()
+    output = _unmark(message)
+    assert output == expected, output
+
+
+def test_send_email():
+    subject = "Test Subject"
+    message = "Test Message"
+
+    email_addresses = [EmailAddressFactory() for _ in range(15)]
+
+    assert mail.outbox == []
+
+    recipients = [email.email for email in email_addresses]
+    send_email(subject, message, recipients)
+
+    assert len(mail.outbox) == 2
+    assert mail.outbox[0].subject == subject
+    assert mail.outbox[0].body == message
+    assert mail.outbox[0].to == [settings.EMAIL_REPLY_TO]
+    assert mail.outbox[0].bcc == recipients[:10]
+
+    assert mail.outbox[1].subject == subject
+    assert mail.outbox[1].body == message
+    assert mail.outbox[0].to == [settings.EMAIL_REPLY_TO]
+    assert mail.outbox[1].bcc == recipients[10:]
